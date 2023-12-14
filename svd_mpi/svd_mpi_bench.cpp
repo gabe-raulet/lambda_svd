2badd7b1fc008c83776edd695600c8b8f7e6779b
Gabriel Raulet <gabe.h.raulet@gmail.com>
2023-12-13 20:12:17 -0800

    compiles and runs so first check. Now need to add timers

---
+++ svd_mpi/svd_mpi_bench.cpp
@@ -0,0 +1,231 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <unistd.h>
+#include <mpi.h>
+#include "kiss.h"
+#include "mmiodense.h"
+#include "svdalgs.h"
+#include "dist_mpi_tree.h"
+#include "cblas.h"
+#include "lapacke.h"
+
+#define BUFSIZE 1024
+
+int random_benchmark(int argc, char *argv[], MPI_Comm comm);
+int stored_benchmark(int argc, char *argv[], MPI_Comm comm);
+void usage(char *argv[]);
+
+int main(int argc, char *argv[])
+{
+    int myrank, nprocs;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+    MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
+
+    if (argc < 2)
+    {
+        if (!myrank) usage(argv);
+        MPI_Finalize();
+        return 1;
+    }
+
+
+    if (!strcmp(argv[1], "random"))
+    {
+        if (random_benchmark(argc, argv, MPI_COMM_WORLD))
+        {
+            MPI_Finalize();
+            return 1;
+        }
+    }
+    else if (!strcmp(argv[1], "stored"))
+    {
+        if (stored_benchmark(argc, argv, MPI_COMM_WORLD))
+        {
+            MPI_Finalize();
+            return 1;
+        }
+    }
+    else
+    {
+        if (!myrank) usage(argv);
+        MPI_Finalize();
+        return 1;
+    }
+
+    MPI_Finalize();
+    return 0;
+}
+
+void usage(char *argv[])
+{
+    fprintf(stderr, "Usage: mpirun -np $nprocs %s <random:stored> ...\n", argv[0]);
+    fprintf(stderr, "random: <p:trunc> <r:rank> <a:mscale> <tall|short>\n");
+    fprintf(stderr, "stored: <p:trunc> <iprefix> <oprefix>\n");
+}
+
+int random_benchmark(int argc, char *argv[], MPI_Comm comm)
+{
+
+    int myrank, nprocs;
+    char const *mattype;
+
+    int m, n, p, r, s, a;
+    double *Aloc, *Utest, *Stest, *Vttest;
+
+    MPI_Comm_size(comm, &nprocs);
+    MPI_Comm_rank(comm, &myrank);
+
+    if (argc != 6)
+    {
+        if (!myrank) fprintf(stderr, "Usage: mpirun -np $nprocs %s [random] <p:trunc> <r:rank> <a:mscale> <tall:wide>\n", argv[0]);
+        return 1;
+    }
+
+    p = atoi(argv[2]);
+    r = atoi(argv[3]);
+    a = atof(argv[4]);
+    mattype = argv[5];
+
+    if (!strcmp(mattype, "tall"))
+    {
+        n = r;
+        m = a*r;
+    }
+    else if (!strcmp(mattype, "wide"))
+    {
+        n = a*r;
+        m = r;
+    }
+    else
+    {
+        assert(0&&"mattype must be tall, or wide");
+    }
+
+    s = n / nprocs;
+
+    assert(n % nprocs == 0);
+    assert(a >= 1);
+    assert(nprocs >= 2);
+    assert(!(nprocs&(nprocs-1)));
+    assert(m >= 1 && n >= 1);
+    assert(!(m&(m-1)));
+    assert(!(n&(n-1)));
+    assert(p <= r);
+    assert(p <= s);
+
+    Aloc = (double*)malloc(m*s*sizeof(double));
+
+    kiss_init();
+
+    for (int i = 0; i < m*s; ++i)
+        Aloc[i] = kiss_unirandf();
+
+    if (!myrank)
+    {
+        Utest = (double*)malloc(m*p*sizeof(double));
+        Stest = (double*)malloc(p*sizeof(double));
+        Vttest = (double*)malloc(p*n*sizeof(double));
+    }
+    else
+    {
+        Utest = Stest = Vttest = NULL;
+    }
+
+    if (dist_mpi_tree(Aloc, Utest, Stest, Vttest, m, n, p, 0, comm))
+    {
+        MPI_Finalize();
+        return 1;
+    }
+
+    free(Aloc);
+
+    if (!myrank)
+    {
+        free(Utest);
+        free(Stest);
+        free(Vttest);
+    }
+
+    return 0;
+}
+
+int stored_benchmark(int argc, char *argv[], MPI_Comm comm)
+{
+    /*
+     * Usage: mpirun -np $nprocs svd_mpi_bench {stored} <p:trunc> <iprefix> <oprefix>
+     */
+
+    assert(0&&"TODO: FIX");
+
+    int myrank, nprocs;
+    char const *iprefix;
+    char const *oprefix;
+    char fname[BUFSIZE];
+    FILE *f;
+
+    int m, n, p, r, s;
+    double *Aloc, *Utest, *Stest, *Vttest;
+
+    MPI_Comm_size(comm, &nprocs);
+    MPI_Comm_rank(comm, &myrank);
+
+    p = atoi(argv[2]);
+    iprefix = argv[3];
+    oprefix = argv[4];
+
+    snprintf(fname, BUFSIZE, "%s_A_%d_%d.mtx", iprefix, myrank+1, nprocs);
+    Aloc = mmread(fname, &m, &s);
+
+    n = s*nprocs;
+    r = m < n? m : n;
+
+    assert(nprocs >= 2);
+    assert(!(nprocs&(nprocs-1)));
+    assert(m >= 1 && n >= 1);
+    assert(!(m&(m-1)));
+    assert(!(n&(n-1)));
+    assert(p <= r);
+    assert(p <= s);
+
+    if (!myrank)
+    {
+        Utest = (double*)malloc(m*p*sizeof(double));
+        Stest = (double*)malloc(p*sizeof(double));
+        Vttest = (double*)malloc(p*n*sizeof(double));
+    }
+    else
+    {
+        Utest = Stest = Vttest = NULL;
+    }
+
+    assert(!dist_mpi_tree(Aloc, Utest, Stest, Vttest, m, n, p, 0, comm));
+    free(Aloc);
+
+    if (!myrank)
+    {
+        snprintf(fname, BUFSIZE, "%s_Utest.mtx", oprefix);
+        mmwrite(fname, Utest, m, p);
+        free(Utest);
+
+        snprintf(fname, BUFSIZE, "%s_Vttest.mtx", oprefix);
+        mmwrite(fname, Vttest, p, n);
+        free(Vttest);
+
+        snprintf(fname, BUFSIZE, "%s_Stest.txt", oprefix);
+
+        f = fopen(fname, "w");
+
+        for (int i = 0; i < p; ++i)
+        {
+            fprintf(f, "%.18e\n", Stest[i]);
+        }
+
+        fclose(f);
+        free(Stest);
+    }
+
+    return 0;
+}
